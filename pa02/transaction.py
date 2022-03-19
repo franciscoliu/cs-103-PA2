145302f02e3f8527db6431fad06533096eb777d8
franciscoliu <zheyuanliu@brandeis.edu>
2022-03-19 11:31:51 -0400

    modify transaction class

--- pa02/transaction.py
+++ pa02/transaction.py
@@ -1,11 +1,30 @@
 import sqlite3
 
+def to_cat_dict(cat_tuple):
+    ''' cat is a category tuple (rowid, name, desc)'''
+    cat = {'rowid':cat_tuple[0], 'item #':cat_tuple[1], 'amount':cat_tuple[2], 'category':cat_tuple[3], 'date':cat_tuple[4], 'description':cat_tuple[5]}
+    return cat
+
+def to_cat_dict_list(cat_tuples):
+    ''' convert a list of category tuples into a list of dictionaries'''
+    return [to_cat_dict(cat) for cat in cat_tuples]
+
 class Transaction():
     def __init__(self,dbfile):
         con = sqlite3.connect(dbfile)
         cur = con.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions 
-                    (item, integer, amount, integer, category, text, date, text, description, text)''')
+                    (item #, integer, amount, integer, category, text, date, text, description, text)''')
         con.commit()
         con.close()
         self.dbfile = dbfile
+        
+    def select_all(self):
+        ''' return all of the categories as a list of dicts.'''
+        con= sqlite3.connect(self.dbfile)
+        cur = con.cursor()
+        cur.execute("SELECT rowid,* from transactions")
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return to_cat_dict_list(tuples)
