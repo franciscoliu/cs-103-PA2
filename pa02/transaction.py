83b26374455c51db573b4798d17daf9bb811817d
Alicia-Sheng <duowensheng@gmail.com>
2022-03-19 21:08:52 -0400

    Fix coding style of transactions.py and raises pylint score to 10/10

--- pa02/transaction.py
+++ pa02/transaction.py
@@ -1,34 +1,41 @@
-import sqlite3
+"""
+Creates a Transaction object which connects with the sqlite database
+to save information such as transactions and categories
+"""
 
+import sqlite3
 
 def to_cat_dict(cat_tuple):
     ''' cat is a category tuple (rowid, name, desc)'''
-    cat = {'rowid': cat_tuple[0], 'item_number': cat_tuple[1], 'amount': cat_tuple[2], 'category': cat_tuple[3],
-           'date': cat_tuple[4], 'description': cat_tuple[5]}
+    cat = {'rowid': cat_tuple[0], 'item_number': cat_tuple[1], 'amount': cat_tuple[2],
+        'category': cat_tuple[3], 'date': cat_tuple[4], 'description': cat_tuple[5]}
     print("Test second")
     return cat
 
 
 def to_cat_dict_list(cat_tuples):
-    ''' convert a list of category tuples into a list of dictionaries'''
+    ''' convert a list of category tuples into a list of dictionaries '''
     return [to_cat_dict(cat) for cat in cat_tuples]
 
 
 def summarize_by_date_helper(cat_tuple):
+    ''' transform list of dates to list of dictionaries '''
     cat = {'date': cat_tuple[0], 'total_amount_transaction': cat_tuple[1]}
     return cat
 
 
 def summarize_by_category_helper(cat_tuple):
+    ''' transform list of categories to list of dictionaries '''
     cat = {'category': cat_tuple[0], 'total_amount_transaction': cat_tuple[1]}
     return cat
 
 
 class Transaction:
+    ''' connects with sqlite and record the information of transactions '''
     def __init__(self, dbfile):
         con = sqlite3.connect(dbfile)
         cur = con.cursor()
-        cur.execute('''CREATE TABLE IF NOT EXISTS transactions 
+        cur.execute('''CREATE TABLE IF NOT EXISTS transactions
                     (item_number text, amount int, category text, date int, description text)''')
         con.commit()
         con.close()
@@ -61,7 +68,8 @@ class Transaction:
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("INSERT INTO transactions VALUES(?,?,?,?,?)",
-                    (item['item_number'], item['amount'], item['category'], item['date'], item['description']))
+                    (item['item_number'], item['amount'], item['category'],
+                    item['date'], item['description']))
         con.commit()
         cur.execute("SELECT last_insert_rowid()")
         last_rowid = cur.fetchone()
@@ -82,6 +90,7 @@ class Transaction:
         con.close()
 
     def summarize_by_date(self):
+        ''' summarizes transactions by date and returns amount sum on each date '''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''SELECT date, SUM(amount) FROM transactions GROUP BY date''')
@@ -91,6 +100,7 @@ class Transaction:
         return [summarize_by_date_helper(tuple) for tuple in tuples]
 
     def summarize_by_month(self):
+        ''' summarizes transactions by month and returns amount sum on each date '''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''SELECT date/100%100, SUM(amount) FROM transactions GROUP BY date/100%100''')
@@ -100,6 +110,7 @@ class Transaction:
         return [summarize_by_date_helper(tuple) for tuple in tuples]
 
     def summarize_by_year(self):
+        ''' summarizes transactions by year and returns amount sum on each date '''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''SELECT date/10000, SUM(amount) FROM transactions GROUP BY date/10000''')
@@ -109,6 +120,7 @@ class Transaction:
         return [summarize_by_date_helper(tuple) for tuple in tuples]
 
     def summarize_by_category(self):
+        ''' summarizes transactions by category and returns amount sum on each date '''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''SELECT category, SUM(amount) FROM transactions GROUP BY category''')
